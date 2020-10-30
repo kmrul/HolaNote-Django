ddf196f68b8cc77ec06d62efdd3b9adbd9296bf5
Kamrul Hasan <khasancsit@gmail.com>
2020-10-31 01:37:13 +0600

    Update settings.py

--- holanote/holanote/settings.py
+++ holanote/holanote/settings.py
@@ -125,7 +125,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'holanoteplus@gmail.com'
-EMAIL_HOST_PASSWORD = 'Hasan@123456'
+EMAIL_HOST_PASSWORD = 'Hasan@******'
 
 # Static files (CSS, JavaScript, Images)
 
